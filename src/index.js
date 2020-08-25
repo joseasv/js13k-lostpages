82d209cb5af4c7104376acec7bbb7af501f43445
Jose Sanchez <joseasv@outlook.com>
2020-08-25 16:55:11 -0400

    - Level data stored in the spritesheet - Showing platform sprite according to level data - Beginning platform controller

--- src/index.js
+++ src/index.js
@@ -14,27 +14,65 @@ context.imageSmoothingEnabled = false;
 initKeys();
 
 let spriteTest = undefined;
-
+let landSprite = [];
 let pc = {
   x: 10,
   y: 10,
   sprite: undefined,
   isMoving: false,
   flipped: false,
+  update: function () {
+    
+    this.isMoving = false;
+    if (keyPressed("left")) {
+      //console.log("pressing left")
+      this.isMoving = true;
+      this.flipped = true;
+      this.x -= 1;
+    }
+
+    if (keyPressed("right")) {
+      //console.log("pressing right")
+      this.isMoving = true;
+      this.flipped = false;
+      this.x += 1;
+    }
+
+    let col = false
+    if (landSprite.length > 0) {
+      for (let i = 0; i < landSprite.length && !col; i++) {
+        let land = landSprite[i]
+        if (this.y + 8 >= land.y && 
+          this.x + 5 >= land.x && this.x + 3 <= land.x + 8) {
+          //console.log("col in sprite", land.id, "this.x", this.x, " :land.x", land.x)
+          col = true
+          this.y = land.y - 8
+        } 
+      }
+    }
+
+    if (!col) {
+      //console.log(col)
+      this.y += 0.5
+    }
+
+    this.sprite.update();
+    
+  },
   render: function () {
     if (this.sprite !== undefined) {
       this.sprite.x = this.x;
       this.sprite.y = this.y;
 
       if (this.isMoving) {
-        console.log("isMoving");
+        //console.log("isMoving");
         this.sprite.playAnimation("walk");
       } else {
         this.sprite.playAnimation("idle");
       }
 
       if (this.flipped) {
-        console.log("FLIP");
+        //console.log("FLIP");
         this.sprite.scaleX = -1;
         this.sprite.x = this.x + this.sprite.width;
       } else {
@@ -42,28 +80,10 @@ let pc = {
       }
       this.sprite.render();
     }
-  },
-  update: function () {
-    if (this.sprite !== undefined) {
-      this.isMoving = false;
-      if (keyPressed("left")) {
-        //console.log("pressing left")
-        this.isMoving = true;
-        this.flipped = true;
-        this.x -= 0.6;
-      }
-
-      if (keyPressed("right")) {
-        //console.log("pressing right")
-        this.isMoving = true;
-        this.flipped = false;
-        this.x += 0.6;
-      }
-
-      this.sprite.update();
-    }
-  },
+  }
 };
+
+let mapcontext = undefined;
 
 const spriteImage = new Image();
 spriteImage.src = spritesheet;
@@ -75,14 +95,14 @@ spriteImage.onload = () => {
     frameHeight: 8,
     animations: {
       walk: {
-        frames: [1, 0],
+        frames: [2, 3],
         frameRate: 5,
         loop: true,
       },
       idle: {
-        frames: 0,
+        frames: [0, 1],
         frameRate: 1,
-        loop: false,
+        loop: true,
       },
     },
   });
@@ -94,65 +114,59 @@ spriteImage.onload = () => {
     height: 8,
     animations: ssheet.animations,
   });
-  spriteTest.context.imageSmoothingEnabled = false
+  spriteTest.context.imageSmoothingEnabled = false;
   //spriteTest.setScale(10, 10)
   pc.sprite = spriteTest;
+
+  const mapcanvas = document.createElement("canvas");
+  mapcontext = mapcanvas.getContext("2d");
+  mapcontext.drawImage(spriteImage, 0, 8, 24, 8, 0, 0, 24, 8);
+
+  const maptilesprites = document.createElement("canvas");
+  const maptilespritescontext = maptilesprites.getContext("2d");
+  maptilespritescontext.drawImage(spriteImage, 32, 0, 8, 8, 0, 0, 8, 8);
+
+  let landId = 0
+  for (let i = 0; i < 24; i++) {
+    for (let j = 0; j < 8; j++) {
+      const pixelData = mapcontext.getImageData(i, j, 1, 1).data;
+      if (pixelData[0] === 245) {
+        //console.log("drawing at ", i, " and ", j);
+      
+        landSprite.push(
+          Sprite({
+            id: landId++,
+            width: 8,
+            height: 8,
+            x: i * 8,
+            y: j * 8,
+            floor: true,
+            image: maptilesprites,
+          })
+        )
+      }
+    }
+  }
+
+  //console.log("landSprite length", landSprite.length);
 };
 
-let sprites = [];
+let aStateId = 0
 
-function createAsteroid() {
-  let asteroid = Sprite({
-    type: "asteroid",
-    x: 50,
-    y: 50,
-    dx: Math.random() * 0.8 - 0.2,
-    dy: Math.random() * 0.8 - 0.2,
-    radius: 12,
-    render() {
-      this.context.strokeStyle = "white";
-      this.context.beginPath();
-      this.context.arc(0, 0, this.radius, 0, Math.PI * 2);
-      this.context.stroke();
-    },
-  });
-
-  sprites.push(asteroid);
-}
-
-for (let i = 0; i < 4; i++) {
-  createAsteroid();
-}
-
-let loop = GameLoop({
+const loop = GameLoop({
   update() {
     pc.update();
-    /*spriteTest.update()
-    if (spriteTest.x > canvas.width) {
-      spriteTest.x = -spriteTest.width
-    }*/
-
-    /*sprites.map(sprite => {
-      sprite.update()
- 
-      if (sprite.x < -sprite.radius) {
-        sprite.x = canvas.width + sprite.radius
-      } else if (sprite.x > canvas.width + sprite.radius) {
-        sprite.x = 0 - sprite.radius
-      }
-
-      if (sprite.y < -sprite.radius) {
-        sprite.y = canvas.height + sprite.radius
-      } else if (sprite.y > canvas.height + sprite.radius) {
-        sprite.y = -sprite.radius
-      }
-    })*/
   },
   render() {
     pc.render();
-    //spriteTest.render()
-    //sprites.map(sprite => sprite.render())
+
+    for (let i = 0; i < landSprite.length; i++) {
+      landSprite[i].render();
+    }
   },
 });
 
-loop.start();
+const states = [loop]
+
+states[aStateId].start();
+
